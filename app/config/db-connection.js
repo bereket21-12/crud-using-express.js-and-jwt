2d909333ef21895dd703187d4faf4e59cf969a07
Bereket Tadele <97612312+bereket21-12@users.noreply.github.com>
2024-09-16 08:37:56 +0300

    Update db-connection.js

--- app/config/db-connection.js
+++ app/config/db-connection.js
@@ -1,4 +1,3 @@
 module.exports = {
-  URL:  "mongodb+srv://berekettadelemulu210:8Zrqw05i9QuMDn9q@crud-with-express.uur3o.mongodb.net/CRUD?retryWrites=true&w=majority&appName=crud-with-express"
-
+  URL: "Your mongodb atlas Connection string"
 }
