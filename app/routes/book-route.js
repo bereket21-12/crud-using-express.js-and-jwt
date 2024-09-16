2676d127f6d83bad1ab17ccfe21a3156e260733e
Bereket Tadele <berekettadelemulu210@gmail.com>
2024-09-16 05:26:58 +0000

    update and delete services added

--- app/routes/book-route.js
+++ app/routes/book-route.js
@@ -2,6 +2,11 @@ const book = require("../service/book-service")
 const jwt = require("../middleware/jwt.auth")
 module.exports = function(app) {
 
-    app.post("/create",jwt.verifyToken,book.create)
+    app.post("/create", jwt.verifyToken, book.create)
+    app.get("/allbook", jwt.verifyToken, book.findAll)
+    app.get("/getbook",jwt.verifyToken,book.findOne)
+    app.put("/updatebook", jwt.verifyToken, book.update);
+    app.delete("/deletebook", jwt.verifyToken, book.delete);
+
     
 }
--- app/service/book-service.js
+++ app/service/book-service.js
@@ -30,6 +30,77 @@ try {
 }
     
 }
+//get all books
+exports.findAll = async (req, res) => {
+  
+  try {
+    const allBook = await Book.find();
+    res.send(allBook)
+  } catch (error) {
+    res.status(500).send({message:"Unable to get books"})
+  }
+  
+}
+
+//get book by id
+exports.findOne = async (req, res) => {
+  validateRequest(req)
+  const id = req.query.id
+  try {
+    const selectedBook = await Book.findById(id);
+    res.send(selectedBook)
+  } catch (error) {
+    res.status(500).send({message:"Unable to get selected book"})
+  }
+  
+}
+
+//update book by id
+exports.update = async (req, res) => {
+  validateRequest(req)
+  const id = req.query.id
+  const updatedBook = {
+    name: req.body.name,
+    page: req.body.page,
+    auther: req.body.page,
+    date:req.body.date
+  }
+  try {
+const updated = await Book.findByIdAndUpdate(id, updatedBook, {
+  new: true, 
+  runValidators: true, 
+});
+
+if (!updated) {
+  return res.status(404).send("Book not found");
+}
+
+res.send(updated);
+  } catch (error) {
+    res.status(500).send({message:"Unable to update the book"})
+  }
+  
+}
+
+exports.delete = async (req, res) => {
+  const id = req.query.id;
+
+  try {
+    const deletedBook = await Book.findByIdAndDelete(id); // Use findByIdAndDelete
+
+    if (!deletedBook) {
+      return res.status(404).send({ message: "Book not found" }); // Send 404 if not found
+    }
+
+    res.status(200).send({ message: "Deleted successfully" }); // Send success response
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: "Error deleting the book", error: error.message }); // Send error response with 500
+  }
+};
+
+
 function validateRequest(req) {
   if (!req.body) {
     res.status(400).send({
